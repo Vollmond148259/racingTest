93254df64425fb817539a83ad4b0aa89861aa590
Vollmond148259 <Vollmond148259@gmail.com>
2023-06-17 19:28:27 +0300

    56

--- src/App.js
+++ src/App.js
@@ -37,70 +37,70 @@ const Plane = (props) => {
   );
 };
 
-const Letter = ({ offset, offsetY, offsetZ, textGeo, mass = 100 }) => {
-  const position = new THREE.Vector3(offset, offsetY, offsetZ);
+// const Letter = ({ offset, offsetY, offsetZ, textGeo, mass = 100 }) => {
+//   const position = new THREE.Vector3(offset, offsetY, offsetZ);
 
-  const [ref] = useBox(() => ({
-    mass,
-    position: [position.x, position.y, position.z],
-    args: [textGeo?.textSize.x, textGeo?.textSize.y, textGeo?.textSize.z / 2]
-  }));
+//   const [ref] = useBox(() => ({
+//     mass,
+//     position: [position.x, position.y, position.z],
+//     args: [textGeo?.textSize.x, textGeo?.textSize.y, textGeo?.textSize.z / 2]
+//   }));
 
-  if (!textGeo) {
-    return null;
-  }
+//   if (!textGeo) {
+//     return null;
+//   }
 
-  return (
-    <mesh ref={ref} castShadow>
-      <primitive object={textGeo?.textGeo} attach="geometry" />
-      <meshLambertMaterial />
-    </mesh>
-  );
-};
+//   return (
+//     <mesh ref={ref} castShadow>
+//       <primitive object={textGeo?.textGeo} attach="geometry" />
+//       <meshLambertMaterial />
+//     </mesh>
+//   );
+// };
 
 // Lotsa letters
-const Text = ({ text, textPosition, size, depth, mass, kerning = DEFAULT_LETTER_SPACING }) => {
-  const font = useLoader(THREE.FontLoader, 'Oleo_Script_Regular.json');
+// const Text = ({ text, textPosition, size, depth, mass, kerning = DEFAULT_LETTER_SPACING }) => {
+//   const font = useLoader(THREE.FontLoader, 'Oleo_Script_Regular.json');
 
-  const config = useMemo(
-    () => ({
-      font,
-      size: size,
-      height: depth,
-      curveSegments: 24,
-      bevelEnabled: false
-    }),
-    [font, size, depth]
-  );
+//   const config = useMemo(
+//     () => ({
+//       font,
+//       size: size,
+//       height: depth,
+//       curveSegments: 24,
+//       bevelEnabled: false
+//     }),
+//     [font, size, depth]
+//   );
 
-  const letterGeometriesArr = text.split('').map((letter) => {
-    const textGeo = new THREE.TextGeometry(letter, config);
-    // You need to manually compute bounding box for geometries
-    textGeo.computeBoundingBox();
-    textGeo.center();
+//   const letterGeometriesArr = text.split('').map((letter) => {
+//     const textGeo = new THREE.TextGeometry(letter, config);
+//     // You need to manually compute bounding box for geometries
+//     textGeo.computeBoundingBox();
+//     textGeo.center();
 
-    // If you have the bounding box (initially null) get its size or else just create a new vector3
-    const textSize = textGeo?.boundingBox?.getSize(new THREE.Vector3()) ?? new THREE.Vector3();
+//     // If you have the bounding box (initially null) get its size or else just create a new vector3
+//     const textSize = textGeo?.boundingBox?.getSize(new THREE.Vector3()) ?? new THREE.Vector3();
 
-    return { textGeo: textGeo, textSize: textSize };
-  });
+//     return { textGeo: textGeo, textSize: textSize };
+//   });
 
-  const textOffsets = letterGeometriesArr.reduce((acc, curr, idx, arr) => {
-    const currLetterWidth = curr?.textSize?.x;
-    const prevLetterWidth = arr[idx - 1]?.textSize?.x ?? 0;
+//   const textOffsets = letterGeometriesArr.reduce((acc, curr, idx, arr) => {
+//     const currLetterWidth = curr?.textSize?.x;
+//     const prevLetterWidth = arr[idx - 1]?.textSize?.x ?? 0;
 
-    // Kerninggggggg
-    const distanceToCenter = idx > 0 ? acc[acc.length - 1] + (currLetterWidth + prevLetterWidth) / 2 + kerning : textPosition.x;
-    acc.push(distanceToCenter);
+//     // Kerninggggggg
+//     const distanceToCenter = idx > 0 ? acc[acc.length - 1] + (currLetterWidth + prevLetterWidth) / 2 + kerning : textPosition.x;
+//     acc.push(distanceToCenter);
 
-    return acc;
-  }, []);
+//     return acc;
+//   }, []);
 
-  return letterGeometriesArr.map((textGeo, idx) => {
-    // All the props lmao
-    return <Letter offsetZ={textPosition.z} offsetY={textPosition.y} offset={textOffsets[idx]} textGeo={textGeo} mass={mass} key={idx} />;
-  });
-};
+//   return letterGeometriesArr.map((textGeo, idx) => {
+//     // All the props lmao
+//     return <Letter offsetZ={textPosition.z} offsetY={textPosition.y} offset={textOffsets[idx]} textGeo={textGeo} mass={mass} key={idx} />;
+//   });
+// };
 
 const Lights = ({ color }) => {
   // color={'#DA2D2D'}
@@ -114,32 +114,32 @@ const Lights = ({ color }) => {
   );
 };
 
-const CursedGateOne = ({ setBgColor, setLightColor, setMessage, setDisplayMessage }) => {
-  return (
-    <Box
-      isTrigger
-      position={[0, 2, 7]}
-      size={[5, 2.5, 1]}
-      wireframe
-      onCollide={(e) => {
-        setBgColor('#FF0000');
-        setLightColor('#FF0000');
-        setMessage('Ok so like this is a work in progress lol');
-        setDisplayMessage(false);
-      }}
-      data={{ name: 'cursed-doorway' }}
-      mass={0}
-      opacity={0}
-    />
-  );
-};
+// const CursedGateOne = ({ setBgColor, setLightColor, setMessage, setDisplayMessage }) => {
+//   return (
+//     <Box
+//       isTrigger
+//       position={[0, 2, 7]}
+//       size={[5, 2.5, 1]}
+//       wireframe
+//       onCollide={(e) => {
+//         setBgColor('#FF0000');
+//         setLightColor('#FF0000');
+//         setMessage('Ok so like this is a work in progress lol');
+//         setDisplayMessage(false);
+//       }}
+//       data={{ name: 'cursed-doorway' }}
+//       mass={0}
+//       opacity={0}
+//     />
+//   );
+// };
 
 export default function App() {
   // will move to zustand state if I feel up to it
   const [bgColor, setBgColor] = useState('#FFB344');
   const [lightColor, setLightColor] = useState('#FFB344');
-  const [message, setMessage] = useState('Welcome to hell');
-  const [displayMessage, setDisplayMessage] = useState(true);
+  // const [message, setMessage] = useState('Welcome to hell');
+  // const [displayMessage, setDisplayMessage] = useState(true);
 
   return (
     <>
@@ -147,20 +147,20 @@ export default function App() {
         <fog attach="fog" args={[bgColor, 10, 50]} />
         <color attach="background" args={[bgColor]} />
         <Lights color={lightColor} />
-        {displayMessage && (
+        {/* {displayMessage && (
           <DreiText position={[0, 0.2, 7]} rotation={[-Math.PI / 2, 0, 0]} fontSize={1}>
             Drive here...
           </DreiText>
-        )}
+        )} */}
         <Physics broadphase="SAP" contactEquationRelaxation={4} friction={1e-3} allowSleep>
           <Suspense fallback={null}>
-            <Text text="Halloween" textPosition={{ x: -9, y: 2, z: -5 }} size={4} depth={1} mass={1} />
-            <Text text="I LOVE U 3 D" textPosition={{ x: -5, y: 2, z: 5 }} size={2} depth={1} mass={1} />
+            {/* <Text text="Halloween" textPosition={{ x: -9, y: 2, z: -5 }} size={4} depth={1} mass={1} />
+            <Text text="I LOVE U 3 D" textPosition={{ x: -5, y: 2, z: 5 }} size={2} depth={1} mass={1} /> */}
 
             <Plane userData={{ id: 'floor' }} rotation={[-Math.PI / 2, 0, 0]} data={{ name: 'floor' }} bgColor={bgColor} />
             <Vehicle position={[0, 2, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 1, 0]} wheelRadius={2} />
-            <Box position={[-5, 2.5, 2]} data={{ name: 'box-1' }} />
-            <Box isTrigger={true} position={[5, 2.5, 3]} data={{ name: 'box-2' }} mass={0} />
+            {/* <Box position={[-5, 2.5, 2]} data={{ name: 'box-1' }} />
+            <Box isTrigger={true} position={[5, 2.5, 3]} data={{ name: 'box-2' }} mass={0} /> */}
             {/* <CursedGateOne setBgColor={setBgColor} setLightColor={setLightColor} setMessage={setMessage} setDisplayMessage={setDisplayMessage} /> */}
           </Suspense>
         </Physics>
@@ -168,7 +168,7 @@ export default function App() {
       </Canvas>
       <Loader />
 
-      <div style={{ position: 'absolute', top: 30, left: 40 }}>
+      {/* <div style={{ position: 'absolute', top: 30, left: 40 }}>
         <h1 className="title">It's Halloween!</h1>
         <div className="controls">
           <p className="controls-content">
@@ -180,7 +180,7 @@ export default function App() {
           </p>
           <p>{message}</p>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
